4ed96e14bd3b83b4746b40d0b1908e0f7867ce99
sandeepbhowmik1 <sandeep.bhowmik@cern.ch>
2019-10-01 08:34:38 +0000

    added di tau producer with dz cut

--- DataFormats/Phase2L1Taus/src/classes.h
+++ DataFormats/Phase2L1Taus/src/classes.h
@@ -1,7 +1,10 @@
 #include "Rtypes.h"
 
+#include "DataFormats/Common/interface/Wrapper.h"  
 #include "DataFormats/Phase2L1Taus/interface/L1HPSPFTau.h"
 #include "DataFormats/Phase2L1Taus/interface/L1HPSPFTauFwd.h"
+#include "DataFormats/Phase2L1Taus/interface/L1HPSPFDiTau.h"
+#include "DataFormats/Phase2L1Taus/interface/L1HPSPFDiTauFwd.h"
 
 namespace DataFormats_Phase2L1Taus
 {
@@ -10,5 +13,13 @@ namespace DataFormats_Phase2L1Taus
     l1t::L1HPSPFTau l1hpspftau;
     l1t::L1HPSPFTauCollection l1hpspftauCollection;
     edm::Wrapper<l1t::L1HPSPFTauCollection> l1hpspftauCWrapper;
+    edm::Ref<l1t::L1HPSPFTauCollection> l1hpspftauCRef;
+    edm::RefVector<l1t::L1HPSPFTauCollection> l1hpspftauCRefVector;
+
+    l1t::L1HPSPFDiTau l1hpspfditau;
+    l1t::L1HPSPFDiTauCollection l1hpspfditauCollection;
+    edm::Wrapper<l1t::L1HPSPFDiTauCollection> l1hpspfditauCWrapper;
+    edm::Ref<l1t::L1HPSPFDiTauCollection> l1hpspfditauCRef;
+    edm::RefVector<l1t::L1HPSPFDiTauCollection> l1hpspfditauCRefVector;
   };
 }
